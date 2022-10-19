3c39ad6f6de3ba7648f7ff5e27c58a3c5926b187
spogrund <pgrsam001@myuct.ac.za>
2022-10-20 01:03:22 +0200

    working with history

--- main.py
+++ main.py
@@ -18,9 +18,10 @@ PORT = 8080
 
 
 class User:
-    receiver_name = 'bob'
+    receiver_name = ''
     key_dict = {}
     sym_key_enc_dict = {}
+    sent_key = False
     def __init__(self):
         self.win = tkinter.Tk()
         self.win.withdraw()
@@ -87,25 +88,33 @@ class User:
                     msg = msg[msg.find(',')+1:]
                     msg = steg.extract(msg)
                     msg = encryption.decrypt_text(msg.encode(), self.sym_key_enc_dict[self.receiver_name])
-                    self.msghist.config(state="normal")
-                    self.msghist.insert(tkinter.END, f"{msg}")
-                    self.msghist.yview(tkinter.END)
-                    self.msghist.config(state="disabled")
+                    if msg[:msg.find(":")] == self.receiver_name:
+                        self.msghist.config(state="normal")
+                        self.msghist.insert(tkinter.END, f"{msg}")
+                        self.msghist.yview(tkinter.END)
+                        self.msghist.config(state="disabled")
                 except:
                     pass
 
     def send_message(self):
         msg = self.msgbox.get("1.0", tkinter.END)
+        self.msgbox.delete("1.0", tkinter.END)
         self.msghist.config(state="normal")
         self.msghist.insert(tkinter.END, f"{self.name}: {msg}")
         self.msghist.yview(tkinter.END)
         self.msghist.config(state="disabled")
         msg = encryption.encrypt_text(f"{self.name}: {msg}", self.sym_key).decode()
-        filename = steg.embedTxt(msg, "audios/sample.wav")
+        filename = steg.embedTxt(msg, "audios/handel.wav")
         msg = f"{self.receiver_name},{filename}"
         msg = encryption.encrypt_text(f"{self.name}: {msg}", self.sym_key).decode()
         self.s.send(msg.encode())
-        self.msgbox.delete("1.0", tkinter.END)
+
+        self.msghist.config(state="normal")
+        msghist = self.msghist.get("1.0", tkinter.END)
+        self.msghist.config(state="disabled")
+
+        msghist_enc = encryption.encrypt_text(f"{msghist}", self.sym_key).decode()
+        steg.embedTxtHist(msghist_enc, "audios/handel.wav", self.name, self.receiver_name)
 
     def get_names(self):
         self.s.send("LON".encode())
@@ -113,8 +122,25 @@ class User:
     def name_select(self, *args):
         self.receiver_name = self.namevar.get().strip(" ")
         #self.s.send(f"new receiver,{self.receiver_name}".encode())
-        self.sym_key_enc = RSA.encrypt(self.sym_key.decode(), self.receiver_name)
-        self.s.send(f"{self.receiver_name},{self.name},key: {self.sym_key_enc}".encode())
+        if self.sent_key == False:
+            self.sym_key_enc = RSA.encrypt(self.sym_key.decode(), self.receiver_name)
+            self.s.send(f"{self.receiver_name},{self.name},key: {self.sym_key_enc}".encode())
+        try:
+            names = [self.name, self.receiver_name]
+            names.sort()
+            filename = f"audios/{names[0]},{names[1]}embedded.wav"
+            print(filename)
+            msg = steg.extract(filename)
+            print("here")
+            msg = encryption.decrypt_text(msg.encode(), self.sym_key_enc_dict[self.receiver_name])
+            print(msg)
+            self.msghist.config(state="normal")
+            self.msghist.delete("1.0", END)
+            self.msghist.insert(tkinter.END, f"{msg}")
+            self.msghist.yview(tkinter.END)
+            self.msghist.config(state="disabled")
+        except:
+            pass
 
 
 if __name__ == '__main__':
--- steg.py
+++ steg.py
@@ -48,3 +48,28 @@ def extract(filename):
     message = message.strip("$")
 
     return message
+
+
+def embedTxtHist(text, file, sender_name, receiver_name):
+    audio = wave.open(file, mode="rb")
+    names = [sender_name, receiver_name]
+    names.sort()
+    msg = text
+    frames = audio.getnframes()
+    audio_bytes = audio.readframes(frames)
+    audio_bytes_list = list(audio_bytes)
+    audio_bytes_array = bytearray(audio_bytes_list)
+    msg += (int((len(audio_bytes_array)-(len(msg)*64))/8))*'$'
+    msg_bits = ''
+    for letter in msg:
+        letter_byte = bin(ord(letter))
+        msg_bits += letter_byte[2:].rjust(8,'0')
+    embedded_message_array = []
+    for i, bit in enumerate(msg_bits):
+        embedded_message_array.append((audio_bytes_array[i] & 254) | int(bit))
+    embedded_byte_array = bytes(embedded_message_array)
+    filename = f"audios/{names[0]},{names[1]}embedded.wav"
+    embedded_audio = wave.open(filename, 'wb')
+    embedded_audio.setparams(audio.getparams())
+    embedded_audio.writeframes(embedded_byte_array)
+    embedded_audio.close()
